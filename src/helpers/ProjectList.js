7b7e3c4822bc1f8adfb77ae88264911f7555c001
Love <lovekumar.200442@gmail.com>
2025-03-17 18:56:15 +0530

    new project added expense and dijktra

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -7,26 +7,28 @@ import Proj6 from "../assets/proj6.jpg";
 import Proj7 from "../assets/proj7.png";
 import Proj8 from "../assets/proj8.jpg";
 
+import expense from "../assets/expense.jpg";
+
 export const ProjectList = [
   // Example entries
-  // {
-  //   name: "Dijkstra Algorithm Visualizer",
-  //   image: Proj1,
-  //   skills: "JavaScript, HTML, CSS",
-  //   link: "https://github.com/yourusername/dijkstra-visualizer"
-  // },
+  {
+    name: "Dijkstra Algorithm Visualizer",
+    image: Proj1,
+    skills: "JavaScript, HTML, CSS",
+    link: "https://path-gamma.vercel.app/"
+  },
   // {
   //   name: "Ecommerce Website",
   //   image: Proj2,
   //   skills: "React, Node.js, MongoDB",
   //   link: "https://github.com/yourusername/ecommerce-website"
   // },
-  // {
-  //   name: "Spotify Clone",
-  //   image: Proj3,
-  //   skills: "React, Node.js, MongoDB, SpotifyAPI",
-  //   link: "https://github.com/yourusername/spotify-clone"
-  // },
+  {
+    name: "expenseTracker",
+    image: expense,
+    skills: "React, Node.js, MongoDB, SpotifyAPI",
+    link: "https://expense-chi-seven.vercel.app/"
+  },
   {
     name: "Social Media Website",
     image: Proj4,
